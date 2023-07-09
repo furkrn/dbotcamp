3dab48d497eb5acbac2d9cc59bd77fa90261cea1
furkan <67959368+furkrn@users.noreply.github.com>
2023-07-09 21:01:19 +0300

    set settings in install

--- background.js
+++ background.js
@@ -3,3 +3,16 @@ import "./background/pagerefreshservice.js";
 import { storageInit } from "./utils/storage.js";
 
 chrome.runtime.onStartup.addListener(async() => storageInit);
+
+chrome.runtime.onInstalled.addListener(() => {
+   chrome.storage.local.set({
+        autorefresh: true,
+        battles: { },
+        includeFree: true,
+        includePaid: false,
+        interval: 100,
+        maximumFee: 0,
+        minimumFee: 0,
+        refreshmin: 10,
+   }); 
+});
